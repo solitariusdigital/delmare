82c299ec5e89c5dd3a9155d677bc6c2f2e4afe6b
Pouyan Madanipour <pouyan.mad@gmail.com>
2022-09-15 19:32:34 +0430

    update: pwa

--- next.config.js
+++ next.config.js
@@ -1,9 +1,23 @@
 const withPWA = require("next-pwa");
 
-module.exports = withPWA({
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development",
+  },
   pwa: {
     dest: "public",
+    disable: process.env.NODE_ENV === "development",
     register: true,
-    skipWaiting: true,
   },
-});
+};
+
+module.exports = () => {
+  const plugins = [withPWA];
+  const config = plugins.reduce((acc, next) => next(acc), {
+    ...nextConfig,
+  });
+  return config;
+};
