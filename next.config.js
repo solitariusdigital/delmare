cb12ef43e98f5062e69f8265c8efde30b7801bb7
Pouyan Madanipour <pouyan.mad@gmail.com>
2022-09-15 19:38:15 +0430

    update: pwa

--- next.config.js
+++ next.config.js
@@ -1,23 +1,38 @@
-const withPWA = require("next-pwa");
+const CircularDependencyPlugin = require("circular-dependency-plugin");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+});
+
+const plugins = [];
+
+if (process.env.ANALYZE === "true") {
+  // only load dependency if env `ANALYZE` was set
+  const withBundleAnalyzer = require("@next/bundle-analyzer")({
+    enabled: true,
+  });
+
+  plugins.push(withBundleAnalyzer);
+}
+
+plugins.push(withPWA);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  compiler: {
-    removeConsole: process.env.NODE_ENV !== "development",
-  },
-  pwa: {
-    dest: "public",
-    disable: process.env.NODE_ENV === "development",
-    register: true,
+  webpack: (config) => {
+    config.plugins.push(
+      new CircularDependencyPlugin({
+        exclude: /a\.js|node_modules/,
+        include: /src/,
+        failOnError: true,
+        allowAsyncCycles: false,
+        cwd: process.cwd(),
+      })
+    );
+
+    return config;
   },
 };
 
-module.exports = () => {
-  const plugins = [withPWA];
-  const config = plugins.reduce((acc, next) => next(acc), {
-    ...nextConfig,
-  });
-  return config;
-};
+module.exports = () => plugins.reduce((acc, next) => next(acc), nextConfig);
