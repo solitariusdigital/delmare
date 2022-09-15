3510d66e8109fcebfb99626f107623158fafdcf3
Pouyan Madanipour <pouyan.mad@gmail.com>
2022-09-15 17:19:07 +0430

    update: landing page anime shuffle

--- components/LandingPage.js
+++ components/LandingPage.js
@@ -1,12 +1,26 @@
-import { Fragment } from "react";
+import { useEffect, useState } from "react";
 import Router from "next/router";
 
 import classes from "./LandingPage.module.scss";
 import Image from "next/image";
-import image from "../assets/home.jpg";
 import ExpandCircleDownIcon from "@mui/icons-material/ExpandCircleDown";
 
+import imageOne from "../assets/one.jpg";
+import imageTwo from "../assets/two.jpg";
+import imageThree from "../assets/three.jpg";
+
 function LandingPage() {
+  const [count, setCount] = useState(0);
+  const imagesArray = [imageOne, imageTwo, imageThree];
+  const image = imagesArray[count % imagesArray.length];
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setCount((count) => count + 1);
+    }, 5000);
+    return () => clearInterval(timerId);
+  }, []);
+
   return (
     <div className={classes.container}>
       <div className={classes.banner} onClick={() => Router.push("/new")}>
@@ -18,6 +32,7 @@ function LandingPage() {
         alt="image"
         layout="fill"
         objectFit="cover"
+        priority="true"
       />
       <div className={classes.message}>
         <p>
