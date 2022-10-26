741b533836c0fc8d020093d3c70a6f3d6029eb30
Pouyan Madanipour <pouyan.mad@gmail.com>
2022-10-26 12:15:10 +0330

    update: env

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const withPWA = require("next-pwa");
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    MONGO_URI: process.env.NEXT_PUBLIC_MONGO_URI,
+  },
 };
 
 module.exports = withPWA({
