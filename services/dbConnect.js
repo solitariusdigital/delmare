302350f61234b411bee093fe4642eb11197390a5
Pouyan Madanipour <pouyan.mad@gmail.com>
2022-10-26 10:21:53 +0330

    update: dbconnection

--- services/dbConnect.js
+++ services/dbConnect.js
@@ -1,8 +1,26 @@
-import mongoose from "mongoose";
+import { connect, connection } from "mongoose";
 
-const dbConnect = async () =>
-  mongoose.connect(process.env.MONGO_URI, {
+const conn = {
+  isConneted: false,
+};
+
+async function dbConnect() {
+  if (conn.isConneted) return;
+
+  const db = await connect(process.env.MONGO_URI, {
     authSource: "admin",
   });
+  conn.isConneted = db.connections[0].readyState;
+
+  console.log(db.connection.db.databaseName);
+}
+
+connection.on("connected", () => {
+  console.log("MongoDB connected to DB");
+});
+
+connection.on("error", (error) => {
+  console.log("MongoDB error", error.message);
+});
 
 export default dbConnect;
