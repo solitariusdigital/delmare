de48018ad8a2b9af94645bb4bcdbf5f796e2ee52
Pouyan Madanipour <pouyan.mad@gmail.com>
2022-11-15 12:34:12 +0330

    update: invoice portal

--- components/Register.js
+++ components/Register.js
@@ -61,6 +61,7 @@ function Register() {
       setAlert("کد تایید ارسال شد");
       startCounter();
       console.log(tokenId);
+      setAlert(tokenId);
 
       // const api = Kavenegar.KavenegarApi({
       //   apikey: process.env.NEXT_PUBLIC_KAVENEGAR,
