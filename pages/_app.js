96cfd1641802d45bb67a9109dd998027924abdbc
Pouyan Madanipour <pouyan.mad@gmail.com>
2023-01-07 09:53:01 +0330

    update: search control

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,6 @@ import "../styles/globals.scss";
 function MyApp({ Component, pageProps }) {
   return (
     <StateProvider>
-      <p>yoyoy</p>
       <Layout>
         <Component {...pageProps} />
       </Layout>
