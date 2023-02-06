464ffe014c0f404727c6f142c52605151688d6a0
Pouyan Madanipour <pouyan.mad@gmail.com>
2023-02-06 15:16:58 +0330

    update: highlight feature

---
+++ components/Highlight.js
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+import { getProducstApi } from "../services/api";
+import classes from "./Highlight.module.scss";
+import Image from "next/image";
+import Router from "next/router";
+
+export default function Highlight() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getProducstApi();
+      setProducts(
+        data
+          .filter((product) => {
+            return product.activate;
+          })
+          .sort((a, b) => {
+            return b.views - a.views;
+          })
+          .slice(0, 5)
+      );
+    };
+    fetchData().catch(console.error);
+  }, []);
+
+  return (
+    <div className={classes.container}>
+      {products.map((product, index) => (
+        <div key={index}>
+          <Image
+            onClick={() =>
+              Router.push(`/collections/product/${product["_id"]}`)
+            }
+            className={classes.image}
+            src={product.images.main}
+            blurDataURL={product.images.main}
+            placeholder="blur"
+            alt="image"
+            width={55}
+            height={55}
+            objectFit="cover"
+            loading="eager"
+            priority
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
--- components/LandingPage.js
+++ components/LandingPage.js
@@ -1,15 +1,17 @@
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useState, useContext, Fragment } from "react";
 import { StateContext } from "../context/stateContext";
 import Router from "next/router";
 import classes from "./LandingPage.module.scss";
 import Image from "next/image";
 import ExpandCircleDownIcon from "@mui/icons-material/ExpandCircleDown";
+import Highlight from "./Highlight";
 
 function LandingPage() {
   const { bar, setBar } = useContext(StateContext);
   const { container, setContainer } = useContext(StateContext);
   const { toggleContainer, setToggleContainer } = useContext(StateContext);
   const [count, setCount] = useState(0);
+
   const sourceLink = `https://delmare.storage.iran.liara.space/landingpage/`;
 
   useEffect(() => {
@@ -55,37 +57,40 @@ function LandingPage() {
   };
 
   return (
-    <div
-      className={classes.container}
-      onClick={() => collections()}
-      onTouchMove={collections}
-    >
-      <div className={classes.banner}>
-        <p>متفاوت بپوشیم</p>
-      </div>
-      <Image
-        className={classes.image}
-        src={assignImage()}
-        blurDataURL={assignImage()}
-        placeholder="blur"
-        alt="image"
-        layout="fill"
-        objectFit="cover"
-        priority
-        loading="eager"
-      />
-      <div className={classes.message}>
-        <p>خرید امن و راحت از بهترین برندهای ایران و دنیا</p>
-        <p>با دلماره متفاوت دیده شوید</p>
-      </div>
-      <div className={classes.icon}>
-        <ExpandCircleDownIcon
-          className="icon"
-          sx={{ color: "#b2ffef", fontSize: 50 }}
-          onClick={() => collections()}
+    <Fragment>
+      <Highlight />
+      <div
+        className={classes.container}
+        onClick={() => collections()}
+        onTouchMove={collections}
+      >
+        <div className={classes.banner}>
+          <p>متفاوت بپوشیم</p>
+        </div>
+        <Image
+          className={classes.image}
+          src={assignImage()}
+          blurDataURL={assignImage()}
+          placeholder="blur"
+          alt="image"
+          layout="fill"
+          objectFit="cover"
+          priority
+          loading="eager"
         />
+        <div className={classes.message}>
+          <p>خرید امن و راحت از بهترین برندهای ایران و دنیا</p>
+          <p>با دلماره متفاوت دیده شوید</p>
+        </div>
+        <div className={classes.icon}>
+          <ExpandCircleDownIcon
+            className="icon"
+            sx={{ color: "#b2ffef", fontSize: 50 }}
+            onClick={() => collections()}
+          />
+        </div>
       </div>
-    </div>
+    </Fragment>
   );
 }
 
