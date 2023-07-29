f977c13082064a8b49bdcd67baeb93b9bdf41fb6
Pouyan Madanipour <pouyan.mad@gmail.com>
2023-07-29 20:19:18 +0330

    fix: load update toggle

--- components/Layout.js
+++ components/Layout.js
@@ -1,18 +1,46 @@
-import { useContext, Fragment } from "react";
+import { useState, useEffect, useContext, Fragment } from "react";
 import { StateContext } from "../context/stateContext";
 import Container from "./navigation/Container";
+import { getNotificationsApi } from "../services/api";
+import Image from "next/image";
+import loadingImage from "../assets/loaderUpdate.png";
+import logo from "../assets/logo.svg";
+import secureLocalStorage from "react-secure-storage";
 
 function Layout(props) {
   const { container, setContainer } = useContext(StateContext);
+  const { userLogIn, setUserLogin } = useContext(StateContext);
+  const [loadAppUpdate, setLoadUpdate] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getNotificationsApi();
+      setLoadUpdate(data[0].update);
+    };
+    fetchData().catch(console.error);
+  }, []);
 
   return (
     <Fragment>
-      {container && (
-        <div className="navigation">
-          <Container />
+      {(userLogIn &&
+        JSON.parse(secureLocalStorage.getItem("currentUser"))["permission"] ===
+          "admin") ||
+      !loadAppUpdate.active ? (
+        <Fragment>
+          {container && (
+            <div className="navigation">
+              <Container />
+            </div>
+          )}
+          <main>{props.children}</main>
+        </Fragment>
+      ) : (
+        <div className="loadAppUpdate">
+          <Image width={120} height={120} src={loadingImage} alt="isLoading" />
+          <p>{loadAppUpdate.text}</p>
+          <Image width={90} height={140} src={logo} alt="logo" />
         </div>
       )}
-      <main>{props.children}</main>
     </Fragment>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,47 +1,24 @@
-import { useState, useEffect } from "react";
 import Layout from "../components/Layout";
 import { StateProvider } from "../context/stateContext";
-import { getNotificationsApi } from "../services/api";
-import loadingImage from "../assets/loaderUpdate.png";
-import Image from "next/image";
-import logo from "../assets/logo.svg";
 import { DefaultSeo } from "next-seo";
 import "../styles/globals.scss";
 
 export default function App({ Component, pageProps }) {
-  const [loadAppUpdate, setLoadUpdate] = useState({});
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await getNotificationsApi();
-      setLoadUpdate(data[0].update);
-    };
-    fetchData().catch(console.error);
-  }, []);
-
   return (
     <StateProvider>
-      {!loadAppUpdate.active ? (
-        <Layout>
-          <DefaultSeo
-            title="با دلماره متفاوت دیده شوید"
-            description="خرید امن و راحت از بهترین برندهای ایران و دنیا"
-            openGraph={{
-              type: "website",
-              locale: "fa_IR",
-              url: "https://delmareh.com/",
-              siteName: "Delmareh",
-            }}
-          />
-          <Component {...pageProps} />
-        </Layout>
-      ) : (
-        <div className="loadAppUpdate">
-          <Image width={120} height={120} src={loadingImage} alt="isLoading" />
-          <p>{loadAppUpdate.text}</p>
-          <Image width={90} height={140} src={logo} alt="logo" />
-        </div>
-      )}
+      <Layout>
+        <DefaultSeo
+          title="با دلماره متفاوت دیده شوید"
+          description="خرید امن و راحت از بهترین برندهای ایران و دنیا"
+          openGraph={{
+            type: "website",
+            locale: "fa_IR",
+            url: "https://delmareh.com/",
+            siteName: "Delmareh",
+          }}
+        />
+        <Component {...pageProps} />
+      </Layout>
     </StateProvider>
   );
 }
